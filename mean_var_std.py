9bbbeda942c87a177b40fa10b3c85f829e32215c
jmirandai1985-dot <jmirandai1985@gmail.com>
2026-05-18 13:35:41 -0400

    Update mean_var_std.py
    Signed-off-by: jmirandai1985-dot <jmirandai1985@gmail.com>

--- mean_var_std.py
+++ mean_var_std.py
@@ -1,42 +1,18 @@
 import numpy as np
 
-def calcular(lista):
-    if len(lista) != 9:
-        raise ValueError("La lista debe contener nueve números.")
+def calculate(list):
+    if len(list) != 9:
+        raise ValueError("List must contain nine numbers.")
     
-    matriz = np.array(lista).reshape(3, 3)
+    matriz = np.array(list).reshape(3, 3)
     
     calculos = {
-        'media': [
-            matriz.mean(axis=0).tolist(), 
-            matriz.mean(axis=1).tolist(), 
-            matriz.flatten().mean().tolist()
-        ],
-        'varianza': [
-            matriz.var(axis=0).tolist(), 
-            matriz.var(axis=1).tolist(), 
-            matriz.flatten().var().tolist()
-        ],
-        'desviacion estandar': [
-            matriz.std(axis=0).tolist(), 
-            matriz.std(axis=1).tolist(), 
-            matriz.flatten().std().tolist()
-        ],
-        'maximo': [
-            matriz.max(axis=0).tolist(), 
-            matriz.max(axis=1).tolist(), 
-            matriz.flatten().max().tolist()
-        ],
-        'minimo': [
-            matriz.min(axis=0).tolist(), 
-            matriz.min(axis=1).tolist(), 
-            matriz.flatten().min().tolist()
-        ],
-        'suma': [
-            matriz.sum(axis=0).tolist(), 
-            matriz.sum(axis=1).tolist(), 
-            matriz.flatten().sum().tolist()
-        ]
+        'mean': [matriz.mean(axis=0).tolist(), matriz.mean(axis=1).tolist(), matriz.flatten().mean().tolist()],
+        'variance': [matriz.var(axis=0).tolist(), matriz.var(axis=1).tolist(), matriz.flatten().var().tolist()],
+        'standard deviation': [matriz.std(axis=0).tolist(), matriz.std(axis=1).tolist(), matriz.flatten().std().tolist()],
+        'max': [matriz.max(axis=0).tolist(), matriz.max(axis=1).tolist(), matriz.flatten().max().tolist()],
+        'min': [matriz.min(axis=0).tolist(), matriz.min(axis=1).tolist(), matriz.flatten().min().tolist()],
+        'sum': [matriz.sum(axis=0).tolist(), matriz.sum(axis=1).tolist(), matriz.flatten().sum().tolist()]
     }
     
     return calculos
